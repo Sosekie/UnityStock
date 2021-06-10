640cb33d6a3dd23afe6ed29c2770f76909b2a8d3
18781361821 <3442137636@qq.com>
2021-06-10 18:59:01 +0800

    first

---
+++ test1/Assets/Scripts/CherryGot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryGot : Collection
+{
+    public override void Death()
+    {
+        base.Death();
+    }
+}
